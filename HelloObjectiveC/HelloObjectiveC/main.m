c1d0570f7d4b9e7c238a968040a75062f3628885
kNagadou <ka.nagadou@gmail.com>
2015-02-09 17:27:14 +0900

    first commit.

---
+++ HelloObjectiveC/HelloObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloObjectiveC
+//
+//  Created by k_nagadou on 2015/02/09.
+//  Copyright (c) 2015年 k_nagadou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
